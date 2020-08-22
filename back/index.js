58df744ff81fbcfeeda5e51e69d62fe8050714f0
Alex Bustos <alexfbp@gmail.com>
2020-08-22 01:59:55 -0500

    enrutamiento basico agregado

--- back/index.js
+++ back/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 // Inicializaciones
 const app = express();
@@ -9,6 +10,12 @@ app.set('port', 5678);
 
 // Middlewares
 app.use(morgan('dev'));
+
+// Enrutamientos
+app.use('/api/teams',require('./routes/equipo'));
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Iniciar el servidor
 app.listen(app.get('port'),()=>{
