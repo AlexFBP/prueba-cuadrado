20fccd3994801a66eb31eac2c272c9baeeb65175
Alex Bustos <alexfbp@gmail.com>
2020-08-22 01:38:33 -0500

    Hot-reloading para desarrollo, y morgan para logs en consola

--- back/index.js
+++ back/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // Inicializaciones
 const app = express();
 
 // Ajustes
 app.set('port', 5678);
+
+// Middlewares
+app.use(morgan('dev'));
 
 // Iniciar el servidor
 app.listen(app.get('port'),()=>{
