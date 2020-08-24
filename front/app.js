5f69a2318ae10a698bf4a5e39b0251b119cf2dd8
Alex Bustos <alexfbp@gmail.com>
2020-08-24 01:57:04 -0500

    agregada comunicacion con la API al front, creacion de equipo validada

--- front/app.js
+++ front/app.js
@@ -1,1 +1,11 @@
 import './styles/app.css';
+
+import {equipoService} from './services/equipoService';
+
+document.getElementById('equipo-form').addEventListener('submit', e => {
+    e.preventDefault();
+    const equipo = document.getElementById('equipoInput').value;
+    // console.log(equipo);
+
+    equipoService.creaEquipo(equipo);
+});
