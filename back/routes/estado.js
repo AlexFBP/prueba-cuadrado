cf6e0aa69ed18db645afa335c249b271ad9f0bea
Alex Bustos <alexfbp@gmail.com>
2020-08-27 22:11:46 -0500

    Back: lectura del estado se puede reusar

--- back/routes/estado.js
+++ back/routes/estado.js
@@ -2,34 +2,46 @@ const { Router } = require("express");
 const router = Router();
 
 const Estado = require('../models/estado');
+const { exists } = require("../models/estado");
 
 const modoValues = ["registro", "jugando"];
 
-router.get('/', async (req,res) => {
-    Estado.findOne({name:"modo"}, (err, doc) => {
-        if (err) {
-            // El error aqui puede ser pero de conexion con la base de datos
-            console.log('error')
-            console.log(err);
-            res.status(500).send(err);
-        } else {
-            if (!doc) {
-                const newEstado = new Estado({name: 'modo', value: 'registro'});
-                newEstado.save();
-                console.log('Estado inicializado');
-                console.log(newEstado);
-                res.send('registro');
+function leeModo () {
+    return new Promise(async (res) => {
+        await Estado.findOne({name:"modo"}, (err, doc) => {
+            if (err) {
+                console.log('Estado-error');
+                res({status: 500,err});
             } else {
-                console.log('Estado leido');
-                res.send(doc.value);
+                if (!doc) {
+                    const newEstado = new Estado({name: 'modo', value: 'registro'});
+                    newEstado.save();
+                    console.log('Estado-inicial');
+                    res({res:'registro'});
+                } else {
+                    console.log('Estado-leido');
+                    res({res:doc.value});
+                }
             }
-        }
-    });
+        });
+    })
+}
+
+router.get('/', async (req,res) => {
+    const objeto = await leeModo();
+    if (objeto.status) {
+        res.status(objeto.status).json(objeto);
+    } else {
+        res.json(objeto);
+    }
 });
 
+const creaPartidos = () => {}
+const puntajesFinales = () => {}
+
 router.post('/', async (req,res) => {
-    const { value } = req.body;
-    if (modoValues.includes(`${value}`)) {
+    const { modo, value } = req.body;
+    if (modo === 'modo' && modoValues.includes(`${value}`)) {
         await Estado.findOneAndUpdate({name:'modo'},{value});
         res.send(req.body.value);
     } else res.status(400).end();
